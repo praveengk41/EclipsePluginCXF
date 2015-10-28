c151f3dfeba6e8f335e06be67d4bd87dc3c98c4b
sateesh_b <sateesh_b@persistent.com>
2015-10-28 11:37:35 +0530

    Rest full WS using Apache cxf

---
+++ src/main/java/com/example/ChangeStudentDetails.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public interface ChangeStudentDetails {
+	Student changeName(Student student);
+	  Student getName();
+
+}
---
+++ src/main/java/com/example/ChangeStudentDetailsImpl.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+
+public class ChangeStudentDetailsImpl implements ChangeStudentDetails {
+
+  @POST
+  @Path("/changeName")
+  @Consumes("application/json")
+  @Produces("application/json")
+  public Student changeName(Student student) {
+    student.setName("HELLO " + student.getName());
+    return student;
+  }
+
+  @GET
+  @Path("/getName")
+  @Consumes("text/plain")
+  @Produces("application/json")
+  public Student getName() {
+    Student student = new Student();
+    student.setName("Rockey");
+    return student;
+  }
+}
